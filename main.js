cda3af350533479ec9be6015b76fa0726244c197
Mahisokhary <114576364+Mahisokhary@users.noreply.github.com>
2023-03-27 09:42:40 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    console.log("made by mahi1sokhary(Qoqnus master)");
+    console.log("i made this website for madsb10");
+    console.log("every thing on this website is for madsb10");
+    console.log("tested on 6.67inch screen with 20:9 aspect ratio");
+
+
+    logo = document.getElementById("logo");
+    logo.onclick = function() {
+        alert("مدزبیتن رو فالو کن زود تند سریییع");
+    }
+}
+$(function() {
+    $(".btn").click(function() {
+        $(".submenu").slideToggle(500);
+    });
+});
